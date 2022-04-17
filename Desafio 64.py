0e4feb0049b97b5960df1e559835364e6ec1ef02
Jefferson Silva <jeffersonsilvaengineer@gmail.com>
2022-04-17 15:02:50 -0300

    Exercicios de python

--- Desafio 64.py
+++ Desafio 64.py
@@ -2,12 +2,13 @@
 digitar o valor de 999, que é a condição de parada. No final mostre quantos números foram digitados
 e qual a soma entre eles, (desconsiderando o flag).'''
 
-cont = 0
-n = ''
+cont = soma = n = 0
+n = int(input('Digite um número: 999 PARA PARAR! '))
 while n != 999:
     cont += 1
-    n = int(input('Digite um número: '))
-    if n == 999:
-        print('FIM!, VOCÊ DIGITOU O NÚMERO {} , DEPOIS DE {} TENTATIVAS!'.format(n,cont))
+    soma += n
+    n = int(input('Digite um número: 999 PARA PARAR! '))
+print('FIM!, VOCÊ DIGITOU {} NÚMEROS E A SOMA ENTRE ELES É {} !'.format(cont,soma))
+
 
 
