8d42101067f0d87ab90a81d50ac99cb2069ec1af
Jefferson Silva <jeffersonsilvaengineer@gmail.com>
2022-04-18 14:10:47 -0300

    Update Desafio 09.py

--- Desafio 09.py
+++ Desafio 09.py
@@ -1,18 +1,18 @@
 '''faça um programa que leia um numero inteiro e mostre sua tabuada!'''
 
 n = int(input('digite um numero!'))
-print('você digitou {}!'.format(n))
-print('A tabuada de {} é!'.format(n))
-print('{} x 01 = \033[41;30m{}\033[m'.format(n, n * 1))
-print('{} x 02 = \033[37;40m{}\033[m'.format(n, n * 2))
-print('{} x 03 = \033[7;36;41m{}\033[m'.format(n, n * 3))
-print('{} x 04 = \033[7;35;42m{}\033[m'.format(n, n * 4))
-print('{} x 05 = \033[7;34;43m{}\033[m'.format(n, n * 5))
-print('{} x 06 = \033[7;33;44m{}\033[m'.format(n, n * 6))
-print('{} x 07 = \033[7;32;45m{}\033[m'.format(n, n * 7))
-print('{} x 08 = \033[7;31;46m{}\033[m'.format(n, n * 8))
-print('{} x 09 = \033[7;30;47m{}\033[m'.format(n, n * 9))
-print('{} x 10 = \033[7;47;32m{}\033[m'.format(n, n * 10))
+print(f'você digitou {n}!'.format(n))
+print(f'A tabuada de {n} é!'.format(n))
+print(f'{n} x 01 = \033[41;30m{n*1}\033[m')
+print(f'{n} x 02 = \033[37;40m{n*2}\033[m')
+print(f'{n} x 03 = \033[7;36;41m{n*3}\033[m')
+print(f'{n} x 04 = \033[7;35;42m{n*4}\033[m')
+print(f'{n} x 05 = \033[7;34;43m{n*5}\033[m')
+print(f'{n} x 06 = \033[7;33;44m{n*6}\033[m')
+print(f'{n} x 07 = \033[7;32;45m{n*7}\033[m')
+print(f'{n} x 08 = \033[7;31;46m{n*8}\033[m')
+print(f'{n} x 09 = \033[7;30;47m{n*9}\033[m')
+print(f'{n} x 10 = \033[7;47;32m{n*10}\033[m')
 
 
 
