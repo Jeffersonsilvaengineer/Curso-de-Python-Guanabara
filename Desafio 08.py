4202e277f8e37605aeeec5275a5f027a7d8ba0be
Jefferson Silva <jeffersonsilvaengineer@gmail.com>
2022-04-18 14:06:27 -0300

    Update Desafio 08.py

--- Desafio 08.py
+++ Desafio 08.py
@@ -1,5 +1,4 @@
 '''crie um progama que leia um valor em metros e converta em centimetros e milimetros!'''
 
 v = int(input('digite um valor!'))
-print('\033[31;46m{}\033[m metros é igual a  \033[30;45m{}\033[m centimetros e igual'
-      ' a \033[42;31m{}\033[m milimetros'.format(v, v*100, v*1000))
+print(f'\033[31;46m{v}\033[m metros é igual a  \033[30;45m{v*100}\033[m centimetros e igual a \033[42;31m{v*1000}\033[m milimetros')
