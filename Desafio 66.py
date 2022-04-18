518d52819dce4b41207205fd7ec04c9b963e896b
Jefferson Silva <jeffersonsilvaengineer@gmail.com>
2022-04-18 19:25:07 -0300

    Exercicios de python

---
+++ Desafio 66.py
@@ -0,0 +1,12 @@
+'''Crie um programa que leia varios números pelo teclado, o programa só deve parar quando o usuário
+digitar o valor de 999, que é a condição de parada. No final mostre quantos números foram digitados
+e qual a soma entre eles, (desconsiderando o flag).'''
+
+cont = soma = n = 0
+while True:
+    soma += n
+    cont += 1
+    n = int(input('Digite um número: 999 PARA PARAR! '))
+    if n == 999:
+        break
+print(f'FIM!, VOCÊ DIGITOU {cont} NÚMEROS E A SOMA ENTRE ELES É {soma} !')
