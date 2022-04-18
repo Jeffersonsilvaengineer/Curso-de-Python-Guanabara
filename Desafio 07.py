2ca638c6c62dc96c56dbf1f6ea177efa6bb90259
Jefferson Silva <jeffersonsilvaengineer@gmail.com>
2022-04-18 14:03:20 -0300

    Update Desafio 07.py

--- Desafio 07.py
+++ Desafio 07.py
@@ -1,7 +1,7 @@
 '''crie um progama que leia duas notas de um aluno e calcule sua média!'''
 n1 = float(input('digite a primeira nota!'))
 n2 = float(input('digite a segunda nota!'))
-print('a média das notas é \033[31;47m{}\033[m!'.format((n1+n2)/2))
+print(f'a média das notas é \033[31;47m{(n1+n2)/2)}\033[m!')
 
 
 
