907d23fa847c6cb50429b3bd49463b1b43c6a1e6
Jefferson Silva <jeffersonsilvaengineer@gmail.com>
2022-04-18 13:36:22 -0300

    Update Desafio 05.py

--- Desafio 05.py
+++ Desafio 05.py
@@ -2,5 +2,5 @@ print('faça um progama que leia um numero e mostre seu antecessor e seu sucesso
 n = int(input('digite um numero!'))
 a = n - 1
 s = n + 1
-print('o numero é \033[7;30;45m{}\033[m seu antecessor é \033[30;44m{}\033[m e seu sucessor é \033[31;42m{}\033[m'.format( n, a, s))
+print(f'o numero é \033[7;30;45m{n}\033[m seu antecessor é \033[30;44m{a}\033[m e seu sucessor é \033[31;42m{s}\033[m')
 
