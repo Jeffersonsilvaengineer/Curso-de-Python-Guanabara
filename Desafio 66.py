48d3c131bbb35933f120cc4b6860d6287dc73cd4
Jefferson Silva <jeffersonsilvaengineer@gmail.com>
2022-04-18 22:01:19 -0300

    Exercicios de python

--- Desafio 66.py
+++ Desafio 66.py
@@ -1,4 +1,4 @@
-'''Crie um programa que leia varios números pelo teclado, o programa só deve parar quando o usuário
+'''Crie um programa que leia vários números pelo teclado, o programa só deve parar quando o usuário
 digitar o valor de 999, que é a condição de parada. No final mostre quantos números foram digitados
 e qual a soma entre eles, (desconsiderando o flag).'''
 
