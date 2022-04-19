060ff3d7dba14d70e85ca5b3d9d45942e9d9044a
Jefferson Silva <jeffersonsilvaengineer@gmail.com>
2022-04-18 21:43:08 -0300

    Exercicios de python

--- Desafio 67.py
+++ Desafio 67.py
@@ -1,3 +1,12 @@
 '''Faça um programa que mostre a tabuada de varios números, um de cada vez, para cada número digitado
 pelo usuário. O programa será interrompido quando o número digitado for negativo.'''
-
+nome = '='
+while True:
+    n = int(input('Quer ver a tabuada de qual valor? '))
+    if n < 0:
+        break
+    print(f'{nome:=^33}')
+    for cont in range(1,11):
+        print(f'{n} X {cont} = {n*cont}')
+    print(f'{nome:=^33}')
+print('Programa encerrado! volte sempre!')
