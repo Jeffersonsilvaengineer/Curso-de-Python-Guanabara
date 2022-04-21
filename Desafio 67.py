6c80c068f4b9fc2a38e7d2cce1e6e0441f36d72b
Jefferson Silva <jeffersonsilvaengineer@gmail.com>
2022-04-21 19:43:02 -0300

    Exercicios de python

--- Desafio 67.py
+++ Desafio 67.py
@@ -1,4 +1,4 @@
-'''Faça um programa que mostre a tabuada de varios números, um de cada vez, para cada número digitado
+'''Faça um programa que mostre a tabuada de vários números, um de cada vez, para cada número digitado
 pelo usuário. O programa será interrompido quando o número digitado for negativo.'''
 
 while True:
---
+++ Desafio 69.py
@@ -0,0 +1,6 @@
+'''Crie um programa que leia a idade e o sexo de várias pessoas. A cada pessoa cadastrada, o programa
+deverá perguntar se o usuário quer ou não continuar. No final mostre:
+Quantas pessoas tem mais de 18 anos.
+Quantos homens foram cadastrados.
+Quantas mulheres tem mais de 20 anos.'''
+
