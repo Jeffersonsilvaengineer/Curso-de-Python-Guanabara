2812db12bad5cb2010b501cb31025a1d8639db67
Jefferson Silva <jeffersonsilvaengineer@gmail.com>
2022-04-22 20:18:24 -0300

    Exercicios de python

--- Desafio 69.py
+++ Desafio 69.py
@@ -4,3 +4,25 @@ Quantas pessoas tem mais de 18 anos.
 Quantos homens foram cadastrados.
 Quantas mulheres tem mais de 20 anos.'''
 
+pessoa = homen = mulher = 0
+while True:
+    print('='*30)
+    idade = int(input('Qual a idade: '))
+    sexo = str(input('Qual o sexo: [M/F]')).upper().strip()
+    continuar = str(input('Quer continuar? [S/N]')).upper().strip()
+    if sexo == 'F':
+        sexo = 'FEMININO'
+    elif sexo == 'M':
+        sexo = 'MASCULINO'
+    if idade > 18:
+        pessoa += 1
+    if sexo == 'MASCULINO':
+        homen += 1
+    if sexo == 'FEMININO' and idade > 20:
+        mulher += 1
+    if continuar == 'N':
+       break
+print('='*10, 'FIM', '='*10)
+print(f'AO TODO TIVEMOS {pessoa} PESSOAS MAIORES DE 18 ANOS!')
+print(f'AO TODO TIVEMOS {homen} HOMENS CADASTRADOS!')
+print(f'AO TODO TIVEMOS {mulher} MULHERES ACIMA DE 20 ANOS!')
