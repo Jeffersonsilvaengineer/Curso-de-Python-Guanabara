4452a9362790cde711051fbce18388a51ef7489f
Jefferson Silva <jeffersonsilvaengineer@gmail.com>
2022-06-04 08:15:36 -0300

    Exercicios de python

---
+++ Desafio 71.py
@@ -0,0 +1,9 @@
+'''Crie um programa que simule o funcionamento de um caixa eletrônico. No inicio pergunte
+ao usuário será o valor a ser sacado (número inteiro) e o programa vai informar quantas cédulas
+de cada valor será entregue. obs: considere que o caixa tenha cédulas de R$50, R$20, R$10 E R$1.'''
+
+print('{:=^30}'.format(' BANCO JEFFERSON '))
+sac = int(input('Que valor você quer sacar? '))
+
+
+    
