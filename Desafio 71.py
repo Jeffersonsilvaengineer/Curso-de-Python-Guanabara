59038c68d1629354f13cb3364eba33505bd4d998
Jefferson Silva <jeffersonsilvaengineer@gmail.com>
2022-09-08 20:32:54 -0300

    Exercicios de python

--- Desafio 71.py
+++ Desafio 71.py
@@ -5,17 +5,37 @@ de cada valor será entregue. obs: considere que o caixa tenha cédulas de R$50,
 print('{:=^30}'.format(' BANCO JEFFERSON '))
 sac = int(input('Que valor você quer sacar? R$ '))
 if sac < 10:
-    d = sac/1
+    d = sac*1
     print(f'O total {d} cedulas de 1 real!')
 elif 10 < sac < 20:
     d1 = sac - 10
-    d = sac/1
+    d = sac*1
     print('O total de 1 cédula de 10 reais')
     print(f'O total de {d1} cédulas de 1 real')
 elif sac == 20:
     print('O total de 1 cédula de 20 reais')
+elif 20 < sac < 30:
+    d3 = sac - 20
+    d4 = d3*1
+    print('O total de 1 cédula de 20 reais')
+    print(f'O total de {d4} cédulas de 1 real')
+elif sac == 30:
+    print('O total de 1 cédula de 20 reais')
+    print('O total de 1 cédula de 10 reais')
+elif 30 < sac < 40:
+    print('O total de 1 cédula de 20 reais')
+    print('O total de 1 cédula de 10 reais')
+    d5 = sac - 30
+    d6 = d5*1
+    print(f'O toal de {d6} cédulas de 1 real')
+elif sac == 40:
+    print('O total de 2 cédulas de 20 reais' )
+elif 40 < sac < 50:
+    print('O total de 2 cédulas de 20 reais')
+    d7 = sac - 40
+    d8 = d7*1
+    print(f'O total de {d8} cédulas de 1 real')
+elif sac == 50:
+    print('O total de 1 cédula de 50 reais')
 
 
-
-
-    
--- Testes das aulas!.py
+++ Testes das aulas!.py
@@ -1,33 +1,3 @@
-import requests
-from tkinter import *
-
-def pegar_cotacoes():
-    requisicao = requests.get("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL")
-
-    requisicao_dic = requisicao.json()
-
-    cotacao_dolar = requisicao_dic['USDBRL']['bid']
-    cotacao_euro = requisicao_dic['EURBRL']['bid']
-    cotacao_btc = requisicao_dic['BTCBRL']['bid']
-
-    texto = f'''
-    Dólar: {cotacao_dolar}
-    Euro: {cotacao_euro}
-    BTC: {cotacao_btc}'''
-    texto_cotacoes['text'] = texto
-    print(texto)
-
-
-janela = Tk()
-janela.title('Cotação do dia!')
-janela.geometry('300x160')
-texto_orientacao = Label(janela, text='Clique no botão para ver as cotaçãoes das moedas')
-texto_orientacao.grid(column=0, row=0, padx=5, pady=5)
-botao = Button(janela, text='Buscar cotações!',command=pegar_cotacoes)
-botao.grid(column=0, row=1, padx=5, pady=5)
-texto_cotacoes = Label(janela, text='')
-texto_cotacoes.grid(column=0, row=2, padx=5, pady=5)
-
-janela.mainloop()
-
-
+n = 30
+d = n//20
+print(d)
