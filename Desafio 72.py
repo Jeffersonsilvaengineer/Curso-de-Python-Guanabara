deda2d1fe83c7fb5f5ea48b171f00e8d1a31d5c7
Jefferson Silva <jeffersonsilvaengineer@gmail.com>
2022-09-10 11:08:54 -0300

    Exercicios de python

--- Desafio 72.py
+++ Desafio 72.py
@@ -1,4 +1,15 @@
+'''Crie um programa que tenha uma tupla totalmente preenchida com uma contagem por extenso de zero até vinte
+seu programa deverá ler um número pelo teclado entre zero e vintee mostralo por extenso.'''
+
 from num2words import num2words
-numero = int(input('Digite um numero! '))
-num_ptbr = num2words(numero, lang='pt-br')
-print(f'você digiou o número {numero} ')
+
+contagem = ('zero','um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez',
+            'onze', 'doze', 'treze', 'catorze', 'qinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
+
+while True:
+     numero = int(input('Digite um numero! '))
+     if 0 <= numero <= 20:
+         break
+     print('TENTE NOVAMENTE!')
+print(f'Você digitou o número {contagem[numero]} ')
+
