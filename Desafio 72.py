e32bca76e5914ca22ad7cfc66bd58f69752b3048
Jefferson Silva <jeffersonsilvaengineer@gmail.com>
2022-09-11 22:17:27 -0300

    Exercicios de python

--- Desafio 72.py
+++ Desafio 72.py
@@ -1,20 +1,17 @@
 '''Crie um programa que tenha uma tupla totalmente preenchida com uma contagem por extenso de zero até vinte
 seu programa deverá ler um número pelo teclado entre zero e vintee mostralo por extenso.'''
 
-from num2words import num2words
-
 contagem = ('zero','um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez',
             'onze', 'doze', 'treze', 'catorze', 'qinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
-
 while True:
-     numero = int(input('Digite um numero! '))
-     if 0 > numero > 20:
-         print('NÚMERO INVÁLIDO!, TENTE NOVAMENTE!')
-     elif 0<= numero <=20:
-         print(f'Você digitou o número {contagem[numero]} ')
-         continuar = str(input('Quer continuar? [S] SIM  [N] NÃO')).strip().upper()
-         if continuar in 'N':
-                break
-print('Obrigado! programa finalizado!')
+    numero = int(input('Digite um numero! '))
+    while 0 > numero > 20:
+        numero2 = int(input('NÚMERO INVÁLIDO!, Digite um numero entre 0 e 20! '))
+        if 0 <= numero2 <= 20:
+            break
+        # print(f'Você digitou o número {contagem[numero]} ')
+    # continuar = str(input('Quer continuar? [S] SIM  [N] NÃO')).strip().upper()
+    # while not continuar in 'SN':
+    #     continuar = str(input('Quer continuar? [S] SIM  [N] NÃO')).strip().upper()
 
 
