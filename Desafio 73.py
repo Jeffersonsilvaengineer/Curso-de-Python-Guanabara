61b83bd1c598207d4f74c9f8398bb60bf43e3427
Jefferson Silva <jeffersonsilvaengineer@gmail.com>
2022-09-16 23:57:40 -0300

    Exercicios de python

---
+++ Desafio 73.py
@@ -0,0 +1,17 @@
+'''Crie uma tupla com os 20 times do campeonato brasileiro na ordem de colocação
+depois mostre:
+A, apenas os 5 primeiros colocados
+B, apenas os 4 ultimos
+C, uma lista com os times em ordem alfabética
+D, em que posição está o time do santos.'''
+tabela = ('PALMEIRAS', 'INTERNACIONAL','FLAMENGO', 'FLUMINENSE','CORINTHIANS', 'ATHLETICO-PR',
+          'ATLETICO-MG', 'AMÉRICA-MG', 'GOIAS', 'SANTOS', 'BRAGANTINO', 'BOTAFOGO', 'SÃO PAULO',
+          'CEARA', 'FORTALEZA', 'CORITIBA', 'CUIABA', 'AVAI', 'ATLETICO-GO', 'JUVENTUDE')
+print('*='*20)
+print('{:^40}'.format('TABELA DO BRASILEIRÃO 2022'))
+print('*='*20)
+print('5 Primeiros colocados =',tabela[:5])
+print('4 últimos colocados =',tabela[-4:])
+print('Classificação em ordem alfabética =\n',sorted(tabela))
+
+
