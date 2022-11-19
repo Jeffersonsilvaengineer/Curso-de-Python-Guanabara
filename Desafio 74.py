b9d8aee1d7895bcf9d570f0ce452e1b55eeb9f55
Jefferson Silva <jeffersonsilvaengineer@gmail.com>
2022-11-19 18:31:37 -0300

    Exercicios de python

--- Desafio 74.py
+++ Desafio 74.py
@@ -2,13 +2,8 @@
 Depois disso, mostre a listagem de números gerados e tambem mostre o menor e o maior numero que estão na tupla.'''
 from random import randint
 
-lis = randint(0,100)
-for c in range(lis):
-    print(c)
+lis = (randint(0,60), randint(0,60), randint(0,60), randint(0,60), randint(0,60))
+love = lis
 print(f'Os valores sorteados foram {lis} ')
-print(f'O maior número sorteado foi {lis}')
-print(f'O menor número sorteado foi {lis}')
-#
-# for c in range(10, -1, -1):
-#     sleep(1)
-#     print(c)
+print(f'O maior número sorteado foi {max(love)}')
+print(f'O menor número sorteado foi {min(love)}')
