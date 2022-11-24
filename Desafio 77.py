e2fe0ba7afdf5acb1a2519092a357713dd2515a9
Jefferson Silva <jeffersonsilvaengineer@gmail.com>
2022-11-24 19:09:19 -0300

    Exercicios de python

---
+++ Desafio 77.py
@@ -0,0 +1,10 @@
+'''Crie um programa que tenha uma tupla com várias palavras, no final mostre todas as palavras e suas vogais'''
+
+tupla = ('amor', 'beijo', 'coraçao', 'aprender', 'python', 'palmeiras', 'casa', 'trabalho', 'motor',
+         'telegrama', 'artistas', 'protetor', 'caramelo', 'morango', 'pescaria', 'praticar', 'ler', 'viver')
+for c in tupla:
+    print(f'\nNa Palavra {c.upper()} temos ', end=' ')
+    for x in c:
+        if x in 'aeiou':
+            print(x, end=' ')
+
