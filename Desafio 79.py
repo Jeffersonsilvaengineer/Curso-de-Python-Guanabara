4a65b4905d7754a12e15275bde494b80e7d10400
Jefferson Silva <jeffersonsilvaengineer@gmail.com>
2022-11-26 19:16:11 -0300

    Exercicios de python

---
+++ Desafio 79.py
@@ -0,0 +1,29 @@
+'''Crie um programa onde o usuário possa digitar vários valores númericos e cadastre em uma lista. Caso
+o número exista la dentro, ele não será adicionado. No final, serão exibidos todos os valores únicos em ordem crescente'''
+
+list = []
+while True:
+    v = int(input('Digite um número...'))
+    if v in list:
+        print('Valor duplicado! digite outro número...')
+        continue
+    list.append(v)
+    print('Valor adicionado com sucesso...')
+    cont = str(input('Quer continuar? [S/N] ')).upper()
+    if cont == 'N':
+        break
+print(list)
+
+
+list = []
+while True:
+    v = int(input('Digite um número...'))
+    if v in list:
+        print('Valor duplicado! digite outro número...')
+    else:
+        list.append(v)
+        print('Valor adicionado com sucesso...')
+        cont = str(input('Quer continuar? [S/N] ')).upper()
+        if cont == 'N':
+            break
+print(list)
