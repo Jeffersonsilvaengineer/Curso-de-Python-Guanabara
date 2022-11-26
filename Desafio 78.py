e72a11fce6325f57e49d34a3f50b0047b7162051
Jefferson Silva <jeffersonsilvaengineer@gmail.com>
2022-11-26 10:26:47 -0300

    Exercicios de python

--- Desafio 78.py
+++ Desafio 78.py
@@ -1,8 +1,12 @@
 '''Faça um programa que leia 5 valores numericos pelo teclado e guarde em uma lista, no final mostre: qual
 foi o maior e o menor valor digitado e suas respectivas posições na lista'''
 
-valores = list()
-for x in range(0,5):
-    valores.append(int(input('Digite um valor: ')))
-print(f'O maior valor foi {max(valores)}, ele aparece na posição {valores.index(max(valores))+1}!')
-print(f'O menor valor digitado foi {min(valores)}, ele aparece na posição {valores.index(min(valores))+1}!')
+valores = []
+for x in range(5):
+    valores.append(int(input(f'Digite um valor na posição {x}: ')))
+
+min_value = min(valores)
+max_value = max(valores)
+
+print(f'O maior valor foi {max_value}, ele aparece na posição {", ".join([str(i) for i, number in enumerate(valores) if number == max_value])}!')
+print(f'O menor valor digitado foi {min_value}, ele aparece na posição {", ".join([str(i) for i, number in enumerate(valores) if number == min_value])}!')
