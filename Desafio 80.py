7de688b8b24d587f6a9add5ce18d9da4f59c805f
Jefferson Silva <jeffersonsilvaengineer@gmail.com>
2022-11-27 14:28:12 -0300

    Exercicios de python

--- Desafio 80.py
+++ Desafio 80.py
@@ -4,7 +4,17 @@ já na posição correta de iserção sem usar o sort(), no final mostre a lista
 list = []
 for c in range(5):
     v = int(input(f'Digite o {c+1}ª número: '))
-    list.append(v)
-print(list)
+    if c == 0 or v > list[-1]:
+        list.append(v)
+        print('Valor adicionado ao final da lista!')
+    else:
+        pos = 0
+        while pos < len(list):
+            if v <= list[pos]:
+                list.insert(pos, v)
+                print(f'Valor adicionado na posiçao {pos}ª da lista!')
+                break
+            pos += 1
+print(f'Os valores digitados em oredem crescente  foram: {list}')
 
 
