9728e3637903772cf33ed6ee3eb7820002fce628
Jefferson Silva <jeffersonsilvaengineer@gmail.com>
2022-12-12 20:51:39 -0300

    Exercicios de python

--- Desafio 83.py
+++ Desafio 83.py
@@ -3,18 +3,18 @@ se a expressão passada está comparêntese abertos e fechados na ordem correta'
 list = []
 list_x = []
 list_y = []
-exp = list.append(input('Digite uma expressão: '))
-for i, c in enumerate(list):
-    if c == '(':
-        list_x.insert(c)
-    elif c == ')':
-        list_y.insert(c)
+expr = str(input('Digite uma expressão: '))
+for simb in expr:
+    if simb == '(':
+        list_x.append('(')
+    elif simb == ')':
+        list_y.append(')')
 print('=-='*12)
-print(f'A expressão digitada foi {list}')
+print(f'A expressão digitada foi {expr}')
 print('=-='*12)
 if len(list_x) == len(list_y):
     print('Sua expressão está correta!')
 else:
     print('Sua expressão está errada!')
-print(list_y)
-print(list_x)
+
+
