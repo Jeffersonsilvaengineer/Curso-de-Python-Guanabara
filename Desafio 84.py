12d72a41dd29bdfe3d7283db79a2720380d3e9a6
Jefferson Silva <jeffersonsilvaengineer@gmail.com>
2022-12-30 14:20:38 -0300

    Update Desafio 84.py

--- Desafio 84.py
+++ Desafio 84.py
@@ -1,50 +1,38 @@
-import random
-from time import sleep
-jogos = int(input('Quantos jogos você deseja? '))
-valor = 0
-for c in range(jogos):
-    list_mega = []
-    while True:
-        numeros = random.randint(1, 60)
-        if numeros in list_mega:
-            continue
-        else:
-            list_mega.append(numeros)
-        list_mega.sort()
-        if len(list_mega) == 20:
-                break
-    sleep(0.1)
-    print('=-=' * 18)
-    print(f'Os números do {c+1}º jogo são --> {list_mega}')
-print('=-='*18)
-if len(list_mega) == 6:
-    valor = 4.5 * jogos
-elif len(list_mega) == 7:
-    valor = 31.5 * jogos
-elif len(list_mega) == 8:
-    valor = 126 * jogos
-elif len(list_mega) == 9:
-    valor = 378 * jogos
-elif len(list_mega) == 10:
-    valor = 945 * jogos
-elif len(list_mega) == 11:
-    valor = 2079 * jogos
-elif len(list_mega) == 12:
-    valor = 4158 * jogos
-elif len(list_mega) == 13:
-    valor = 7722 * jogos
-elif len(list_mega) == 14:
-    valor = 13513.50 * jogos
-elif len(list_mega) == 15:
-    valor = 22522.50 * jogos
-elif len(list_mega) == 16:
-    valor = 36036 * jogos
-elif len(list_mega) == 17:
-    valor = 55692 * jogos
-elif len(list_mega) == 18:
-    valor = 83538 * jogos
-elif len(list_mega) == 19:
-    valor = 122094 * jogos
-elif len(list_mega) == 20:
-    valor = 174420 * jogos
-print(f'O VALOR TOTAL DESTE BOLÃO É R$ {valor :0,} REAIS')
+'''Crie um programa que leias varios nomes e pesos e gurde tudo em uma lita, no final mostre:
+A)Quantas pessoas foram cadastradas!
+B)Qual o pesso e nome da pessoa mais pesada!
+C)Qual o nome e peso da pessoa mais leve!'''
+
+list_temp = []
+list_total = []
+maior = menor = 0
+while True:
+    list_temp.append(str(input('NOME: ')))
+    list_temp.append(float(input('PESO: ')))
+    if len(list_total) == 0:
+        maior = menor = list_temp[1]
+    else:
+        if list_temp[1] > maior:
+            maior = list_temp[1]
+        if list_temp[1] < menor:
+            menor = list_temp[1]
+    list_total.append(list_temp[:])
+    list_temp.clear()
+    continuar = str(input('Quer continuar? [S/N]: ')).upper()
+    while continuar not in 'SN':
+        continuar = str(input('RESPOSTA INVÁLIDA! Quer continuar? [S/N]: ')).upper()
+    if continuar == 'N':
+        break
+print(f'ESSAS FORAM TODAS AS PESSOAS CADASTRADAS --> {list_total}')
+print(f'AO TODO FORAM CADASTRADAS {len(list_total)} PESSOAS!')
+print(f'O MAIOR PESO FOI {maior}KG, E PERTENÇE A -->', end=' ')
+for p in list_total:
+    if p[1] == maior:
+        print(f'{p[0]}', end=' ')
+print()
+print(f'O MENOR PESO FOI {menor}KG, E PERTENÇE A -->', end=' ')
+for p in list_total:
+    if p[1] == menor:
+        print(f'{p[0]}', end=' ')
+print()
+
