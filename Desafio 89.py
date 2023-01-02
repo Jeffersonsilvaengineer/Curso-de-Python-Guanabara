dff72f9bfe8a3dec28b35eacb5c78e94e97f65d4
Jefferson Silva <jeffersonsilvaengineer@gmail.com>
2023-01-02 10:45:07 -0300

    Update Desafio 89.py

--- Desafio 89.py
+++ Desafio 89.py
@@ -1,11 +1,1 @@
-semana = ['Domingo', 'Segunda feira', 'Terça feira', 'Quarta feira', 'Quinta feira', 'Sexta feira', 'Sabado']
-#va1 = int(input('Digite um dia da semana: '))
-va2 = int(input('Digite um valor: '))
-
-z = va2 % 3
-
-print(z)
-
-
-
 
