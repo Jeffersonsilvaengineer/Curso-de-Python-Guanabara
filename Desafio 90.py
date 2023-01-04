41cc7f61ffde423213c020ed32ad1db1449232f1
Jefferson Silva <jeffersonsilvaengineer@gmail.com>
2023-01-04 19:09:32 -0300

    Exercicios de python

---
+++ Desafio 90.py
@@ -0,0 +1,14 @@
+'''Faça um programa que leia nome e média de um aluno guardando também sua situação em um dicionário
+No final mostre o conteúdo da estrutura na tela.'''
+
+aluno = dict()
+aluno['Nome'] = str(input('NOME: '))
+aluno['Média'] = float(input(f'Média de {aluno["Nome"]}: '))
+if aluno['Média'] >= 7:
+    aluno['Situação'] = 'Aprovado'
+elif aluno['Média'] < 5:
+    aluno['Situação'] = 'Reprovado'
+else:
+    aluno['Situação'] = 'Recuperação'
+for k, v in aluno.items():
+    print(f'{k} é igual a {v}')
