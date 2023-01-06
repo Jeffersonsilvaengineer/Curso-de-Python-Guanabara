093f06002e7ce4a8dfe1af39288325b75d22d949
Jefferson Silva <jeffersonsilvaengineer@gmail.com>
2023-01-05 21:12:12 -0300

    Exercicios de python

--- Desafio 02.py
+++ Desafio 02.py
@@ -1,3 +1,5 @@
 '''Crie um programa que pergunte o nome da pessoa e mostre uma saudação!'''
-nome = str(input('Digite seu nome!'))
+
+nome = str(input('Digite seu nome! '))
+print('=-' * 20)
 print(f'É um prazer te conhecer, {nome}!')
