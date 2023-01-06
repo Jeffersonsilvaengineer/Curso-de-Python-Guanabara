2475514557e7087054740b3c4bdfe5bab23d9d48
Jefferson Silva <jeffersonsilvaengineer@gmail.com>
2023-01-05 21:13:22 -0300

    Exercicios de python

--- Desafio 03.py
+++ Desafio 03.py
@@ -2,4 +2,5 @@
 
 pv = int(input('Digite um número!'))
 sv = int(input('Digite outro número!'))
-print(f'A soma de {pv} e {sv} é {pv+sv}!')
+print(f'A soma de {pv} e {sv} é {pv + sv}!')
+print('FIM')
