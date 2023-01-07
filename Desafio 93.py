f5f9d9eee574eb3da1be8ecd94fd9b8be7fbffa4
Jefferson Silva <jeffersonsilvaengineer@gmail.com>
2023-01-07 16:29:30 -0300

    Exercicios de python

--- Desafio 93.py
+++ Desafio 93.py
@@ -2,3 +2,23 @@
 O programa vai ler o nome do jogador e quantas partidas ele jogou.
 Depois vai ler a quantidade de gols feitos em cada partida. No final, tudo isso será guardado em um dicionário,
 incluindo o total de gols feitos durante o campeonato.'''
+jogador = dict()
+gols = list()
+soma = 0
+jogador['Nome'] = str(input('Nome: '))
+jogador['Partidas'] = int(input(f'Quantas partidas {jogador["Nome"]} jogou: '))
+for c in range(jogador['Partidas']):
+     gols.append(int(input(f'Quantos gols {jogador["Nome"]} marcou na {c + 1}ºPartida: ')))
+     jogador['Gols'] = gols
+for gol in gols:
+     soma += gol
+jogador['Total DE GOLS'] = soma
+print('=-' * 30)
+print(jogador)
+print('=-' * 30)
+for k, v in jogador.items():
+     print(f'O campo {k} tem valor {v}')
+print('=-' * 30)
+print(f'O jogador {jogador["Nome"]} jogou {jogador["Partidas"]} partidas.')
+for c in range(jogador["Partidas"]):
+     print(f'=> Na partida {c + 1}, fez {jogador["Gols"][c]} gols.')
