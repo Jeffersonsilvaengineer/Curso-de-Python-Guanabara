102ac4e8ac746a895066d394a497fd9fd09d26df
Jefferson Silva <jeffersonsilvaengineer@gmail.com>
2023-01-07 13:12:15 -0300

    Exercicios de python

--- Desafio 92.py
+++ Desafio 92.py
@@ -2,13 +2,11 @@
 Se por acaso a CTPS for diferente de ZERO, o dicionário receberá também o ano de contratação e o salário.
 Calcule e acrescente, além da idade, com quantos anos a pessoa vai se aposentar.'''
 from datetime import datetime
-
 pessoa = dict()
 while True:
     pessoa['Nome: '] = str(input('Nome: '))
     ano = int(input('Ano de nascimento: '))
-    x = datetime.now()
-    idade = x.year - ano
+    idade = datetime.now().year - ano
     pessoa['Idade: '] = idade
     pessoa['Ctps: '] = int(input('Carteira de trabalho [0 não tem!]: '))
     if pessoa['Ctps: '] == 0:
@@ -19,4 +17,4 @@ while True:
     pessoa['Salário: '] = float(input('Salário: R$'))
     break
 for k, v in pessoa.items():
-    print(f'{k} tem o valor {v}')
+    print(f'- {k:>15} tem o valor {v}')
