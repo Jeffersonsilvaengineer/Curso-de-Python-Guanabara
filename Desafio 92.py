23cfa23cd44298695c8aad7a18f9af4b511f47fd
Jefferson Silva <jeffersonsilvaengineer@gmail.com>
2023-01-07 12:54:36 -0300

    Exercicios de python

--- Desafio 92.py
+++ Desafio 92.py
@@ -1,3 +1,22 @@
 '''Crie um programa que leia nome, ano de nascimento e carteira de trabalho e cadastre-o (com idade) em um dicionário.
 Se por acaso a CTPS for diferente de ZERO, o dicionário receberá também o ano de contratação e o salário.
 Calcule e acrescente, além da idade, com quantos anos a pessoa vai se aposentar.'''
+from datetime import datetime
+
+pessoa = dict()
+while True:
+    pessoa['Nome: '] = str(input('Nome: '))
+    ano = int(input('Ano de nascimento: '))
+    x = datetime.now()
+    idade = x.year - ano
+    pessoa['Idade: '] = idade
+    pessoa['Ctps: '] = int(input('Carteira de trabalho [0 não tem!]: '))
+    if pessoa['Ctps: '] == 0:
+        break
+    pessoa['Contratação: '] = int(input('Ano de contratação: '))
+    y = (pessoa['Contratação: ']) - ano + 35
+    pessoa['Aposentadoria: '] = y
+    pessoa['Salário: '] = float(input('Salário: R$'))
+    break
+for k, v in pessoa.items():
+    print(f'{k} tem o valor {v}')
