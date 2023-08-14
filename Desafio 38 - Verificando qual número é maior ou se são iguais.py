79f9fb324287da7a3ae3c4c459105d4ae3ef021d
Jefferson Silva <jeffersonsilvaengineer@gmail.com>
2023-08-13 22:38:59 -0300

    Exercicios de python

--- Desafio 38 - Verificando qual número é maior ou se são iguais.py
+++ Desafio 38 - Verificando qual número é maior ou se são iguais.py
@@ -1,4 +1,4 @@
-'''Faça um progama que leia dois números inteiros e compareos mostrando na tela as seguintes informações: o primeiro
+'''Faça um programa que leia dois números inteiros e compareos mostrando na tela as seguintes informações: o primeiro
 número é maior, o segundo número é maior, não existe número maior os dois são iguais'''
 
 p = int(input('Digite o primeiro número!'))
