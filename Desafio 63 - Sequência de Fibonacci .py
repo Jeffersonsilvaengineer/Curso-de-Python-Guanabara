3b1f79451bbfc414844ad58198bdeb6ab7b39b07
Jefferson Silva <jeffersonsilvaengineer@gmail.com>
2023-10-28 10:05:12 -0300

    Exercicios de python

--- Desafio 63 - Sequência de Fibonacci .py
+++ Desafio 63 - Sequência de Fibonacci .py
@@ -1,13 +1,14 @@
 '''crie um programa que leia um número inteiro qualquer e moastre na tela sua sequência de fibonacci!'''
 
-n = int(input('Digite um número qualquer!'))
+n = int(input('Digite um número qualquer! '))
 t1 = 0
 t2 = 1
 cont = 3
-print('{} -> {} -> '.format(t1, t2), end='')
+print(f'{t1} -> {t2} -> ', end='')
+
 while cont <= n:
     t3 = t1 + t2
-    print('{} -> '.format(t3), end='')
+    print(f'{t3} -> ', end='')
     cont += 1
     t1 = t2
     t2 = t3
