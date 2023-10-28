39eb1cb09094039add7d2cc181197993b9150dbd
Jefferson Silva <jeffersonsilvaengineer@gmail.com>
2023-10-28 10:17:46 -0300

    Exercicios de python

--- Desafio 66 - Condição de Parada com Flag.py
+++ Desafio 66 - Condição de Parada com Flag.py
@@ -3,6 +3,7 @@ digitar o valor de 999, que é a condição de parada. No final mostre quantos n
 e qual a soma entre eles, (desconsiderando o flag).'''
 
 cont = soma = n = 0
+
 while True:
     n = int(input('Digite um número: 999 PARA PARAR! '))
     if n == 999:
