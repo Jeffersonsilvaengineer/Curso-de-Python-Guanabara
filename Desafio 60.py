9f68e8b30301deb0a2a0c902e3dd509f066e3c46
Jefferson Silva <jeffersonsilvaengineer@gmail.com>
2023-10-28 09:55:21 -0300

    Exercicios de python

--- Desafio 60.py
+++ Desafio 60.py
@@ -1,11 +1,13 @@
 '''Faça um programa que leia um número interiro e mostre sua fatorial'''
+
 from time import sleep
-num = int(input('Digite um número: '))
-fat = 1
-c = num
+
+numero = int(input('Digite um número: '))
+fatorial = 1
+c = numero
 while c > 0:
-    sleep(0.5)
-    print(' {} '.format(c),end='')
-    print('x' if c > 1 else ' = '' {} '.format(fat),end=''.format(c,))
-    fat *= c
+    sleep(0.3)
+    print(f' {c} ', end='')
+    print('x' if c > 1 else ' = '' {} '.format(fatorial), end=''.format(c, ))
+    fatorial *= c
     c -= 1
