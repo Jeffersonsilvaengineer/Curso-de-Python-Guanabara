c8a3460b8b71c66d3fb75be92cc2f7508791f852
Jefferson Silva <jeffersonsilvaengineer@gmail.com>
2024-01-17 15:55:34 -0300

    Exercicios de python

--- Desafio 74 - Maior x Menor dentro da tupla.py
+++ Desafio 74 - Maior x Menor dentro da tupla.py
@@ -1,5 +1,6 @@
 '''Crie um programa que vai gerar 5 números aleatórios e colocar dentro de uma tupla.
 Depois disso, mostre a listagem de números gerados e tambem mostre o menor e o maior numero que estão na tupla.'''
+
 from random import randint
 
 lis = (randint(0,60), randint(0,60), randint(0,60), randint(0,60), randint(0,60))
--- Desafio 75 - Números dentro de uma tupla.py
+++ Desafio 75 - Números dentro de uma tupla.py
@@ -1,4 +1,4 @@
-'''Desenvolva um programa que leia quatro valores pelo teclado e guadeos em uma tupla, no final mostre:
+'''Desenvolva um programa que leia quatro valores pelo teclado e guardeos em uma tupla, no final mostre:
 
 Quantas vezes aparece o valor 9!
 Em que posição foi digitado pela primeira vez o valor 3!
@@ -18,7 +18,7 @@ if 3 in num:
        print(f'O número 3 apareceu pela primeira vez na {num.index(3)+1}ª posição! ')
 else:
        print('O número 3 não foi digitado')
-print('Os números pares foram: ', end=''  )
+print('Os números pares foram: ', end='')
 for par in num:
        if par % 2 == 0:
               print(par, end=' ')
